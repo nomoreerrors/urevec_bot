fc3a04268525faa8f163755569a187773e138ed2
Денис Астафьев <nomoreerrors@Deniss-MacBook-Pro.local>
2024-06-21 13:54:56 +0300

    push to hosting 13:54 june 21

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -7,21 +7,31 @@ use Illuminate\Support\Facades\Http;
 
 class TelegramBotController extends Controller
 {
+
+    private $message = "where is my mind?";
+
+
     public function getUpdates()
     {
 
-        $apiUrl = "https://api.telegram.org/bot";
-        $token = "7239017745:AAGSwLlNc1MTkanpm0w7vBGegQU_o54ptrw";
-        $message = "where is my mind?";
-        // $message = urlencode($message);
 
-        $userId = "754429643"; //здесь может быть id тг-чата
-        Http::get($apiUrl . $token . "/sendMessage", ["chat_id" => $userId, "text" => $message]);
+
+        Http::get(
+            env('TELEGRAM_API_URL') . env('TELEGRAM_API_TOKEN') . "/sendMessage",
+            ["chat_id" => env('TELEGRAM_API_TEST_USER_ID'), "text" => $this->message]
+        );
     }
 
 
-    public function setWebHook()
+    public function setWebHook(Request $request)
     {
-        dd('Lolwut"');
+        Http::get(
+            env('TELEGRAM_API_URL') . env('TELEGRAM_API_TOKEN') . "/setWebhook",
+            ["chat_id" => env('TELEGRAM_API_TEST_USER_ID'), "text" => $this->message]
+        );
+
+        $data = $request->getContent();
+        dd($data);
+        // dd('Lolwut"');
     }
 }
