15e9f513b70b2530054c49992ba6c708f956907e
Денис Астафьев <nomoreerrors@Deniss-MacBook-Pro.local>
2024-06-21 13:19:22 +0300

    second one

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\TelegramBotController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
-Route::get('/test', [TelegramBotController::class, 'getUpdates']);
-// Route::patch('posts/{id}/restore', [BlogPostController::class, 'restore'])
-//     ->name('blog.admin.posts.restore');
-Route::get('/hook', [TelegramBotController::class, 'setWebHook']);
+Route::get('/get_updates', [TelegramBotController::class, 'getUpdates']);
+
+//эквивалентно bot_urevec url на хостинге
+Route::get('/', [TelegramBotController::class, 'setWebHook']);
